eff1dd78f7814028edd324cd9050d844a0274a6f
ramanujdas <ramanujdas@localhost>
2022-07-25 14:46:46 +0530

    Updated Application.properties

---
+++ del-bangalore-spring-ms/foodie-spring-mvc-app.zip_expanded/foodie-spring-mvc-app/src/main/java/com/del/foodieapp/FoodieSpringMvcAppApplication.java
@@ -0,0 +1,13 @@
+package com.del.foodieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieSpringMvcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieSpringMvcAppApplication.class, args);
+	}
+
+}
